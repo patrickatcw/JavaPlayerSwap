54db12e91a8401ad4d36e3bdc316560b1a986730
patrickatcw <pj8392@yahoo.com>
2018-02-19 17:50:11 -0500

    fields, constructor, references created, work on getters next using code in menu to construct

--- src/com/me/Player.java
+++ src/com/me/Player.java
@@ -1,4 +1,27 @@
 package com.me;
 
 public class Player {
+
+    //step 1 create fields for Player
+    private String player1;
+    private String player2;
+    private String player3;
+    private String player4;
+    private String player5;
+
+    //step 2 make my own constructor method with parameters
+    public Player(String player1, String player2, String player3,
+                  String player4, String player5){
+
+        //step 3 this. to reference fields
+        this.player1 = player1;
+        this.player2 = player2;
+        this.player3 = player3;
+        this.player4 = player4;
+        this.player5 = player5;
+
+    }
+
+    //step 4 create getters
+
 }
