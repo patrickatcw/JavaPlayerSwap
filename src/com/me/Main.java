02d0c0fa321b857259ae8e1e48f3a822c9f6838a
patrickatcw <pj8392@yahoo.com>
2018-02-19 20:08:52 -0500

    printout for team roster created, work on player swap methods next

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -16,7 +16,21 @@ public class Main {
         Team purple = new Team("Pete", "Pam", "Pandit", "Phun-shu", "Pogo");
 
         //step 6 do printouts
-
+        System.out.println("-------------------------------------------------");
+        System.out.println("Here are the team rosters for the player swap;");
+        System.out.println();
+        System.out.println("Team Blue: " + blue.getPlayer1() + ", " + blue.getPlayer2() + ", " + blue.getPlayer3()
+                + ", " + blue.getPlayer4() + ", " + blue.getPlayer5());
+        System.out.println("Team Red: " + red.getPlayer1() + ", " + red.getPlayer2() + ", " + red.getPlayer3()
+                + ", " + red.getPlayer4() + ", " + red.getPlayer5());
+        System.out.println("Team Orange: " + orange.getPlayer1() + ", " + orange.getPlayer2() + ", " + orange.getPlayer3()
+                + ", " + orange.getPlayer4() + ", " + orange.getPlayer5());
+        System.out.println("Team Neon: " + neon.getPlayer1() + ", " + neon.getPlayer2() + ", " + neon.getPlayer3()
+                + ", " + neon.getPlayer4() + ", " + neon.getPlayer5());
+        System.out.println("Team Purple: " + purple.getPlayer1() + ", " + purple.getPlayer2() + ", " + purple.getPlayer3()
+                + ", " + purple.getPlayer4() + ", " + purple.getPlayer5());
+        System.out.println("-------------------------------------------------");
+        System.out.println();
 
     }
 
