058acc80d50e5738cabb6a0b02596b4d3ca88c40
patrickatcw <pj8392@yahoo.com>
2018-02-19 19:07:15 -0500

    made instances work on printouts next for teams rosters

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -9,6 +9,13 @@ public class Main {
     public static void main(String[] args) {
 
         //step 5 building new instances using constructors
+        Team blue = new Team("Bob", "Bill", "Bo", "Ben", "Buck");
+        Team red = new Team("Rick", "Rob", "Rita", "Renee", "Rudy");
+        Team orange = new Team("Otter", "Othello", "Oscar", "Octavian", "Oren");
+        Team neon = new Team("Nancy", "Ned", "Nelly", "Nick", "Natalie");
+        Team purple = new Team("Pete", "Pam", "Pandit", "Phun-shu", "Pogo");
+
+        //step 6 do printouts
 
 
     }
